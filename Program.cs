1384f20f5f39598cc7c920238be431c75d1236d6
Jettel17 <etteljake@gmail.com>
2025-01-21 13:46:30 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,59 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collection.Generic;
+
+public class playingCard
+{
+    public string Suit { get; set; }
+    public string Value { get; set; }
+
+    public playingCard() : this("Joker", "Joker")
+    {
+    }
+
+    public playingCard(string suit, string value)
+    {
+        Suit = suit;
+        Value = value;
+    }
+
+    public void printCard()
+    {
+        Console.WriteLine($"{Value} of {Suit}")
+    }
+}
+
+
+public class deck
+{
+    public list<playingCard> cards;
+    public deck()
+    {
+        cards = new List<playingCard>();
+        startDeck();
+    }
+    public void startDeck()
+    {
+        string suits = { "Hearts", "Diamonds", "Clubs", "Spades" };
+        string values = { "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace" };
+
+        foreach (var suit in suits)
+        {
+            foreach (var value in values)
+            {
+                cards.Add(new playingCard(suit, value));
+            }
+        }
+
+        cards.Add(new playingCard());
+    }
+
+    public playingCard Draw()
+    {
+        if (cards.Count == 0)
+        {
+            throw new 
+        }
+    }
+}
+
+
